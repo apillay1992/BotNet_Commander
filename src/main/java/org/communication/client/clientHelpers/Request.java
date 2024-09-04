06d09054053f3ba683621f97d41ee68f5f0a24de
apillay023 <apillay023@student.wethinkcode.co.za>
2024-09-04 08:53:37 +0200

    added four variable and created two constructors

--- src/main/java/org/communication/client/clientHelpers/Request.java
+++ src/main/java/org/communication/client/clientHelpers/Request.java
@@ -1,4 +1,26 @@
 package org.communication.client.clientHelpers;
 
+import com.google.gson.Gson;
+import org.communication.server.serverHelpers.robot.Robot;
+
 public class Request {
+    private String robotName;
+    private String command;
+    private String[] arguments;
+    private Robot robot;
+
+    //Constructs an empty Request.
+    public Request(){
+    }
+
+    /**
+     * Constructs a new Request with the specified robot name and command.
+     *
+     * @param robotName The name of the robot.
+     * @param command The command to be executed.
+     */
+    public Request(String robotName, String command){
+        this.robotName = robotName;
+        this.command = command;
+    }
 }
