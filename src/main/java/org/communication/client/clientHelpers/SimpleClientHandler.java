03a8f9439ff24fe7dc4ed79ba92822daba4acadf
apillay023 <apillay023@student.wethinkcode.co.za>
2024-09-06 09:10:34 +0200

    added a method to handle the users Input

--- src/main/java/org/communication/client/clientHelpers/SimpleClientHandler.java
+++ src/main/java/org/communication/client/clientHelpers/SimpleClientHandler.java
@@ -7,6 +7,7 @@ import org.communication.client.Client;
 import org.communication.server.serverHelpers.robotModels.*;
 
 import static org.communication.server.serverHelpers.headers.DisplayHeaders.*;
+import static org.communication.server.serverHelpers.serverHandler.SimpleServer.validCommands;
 import static org.communication.server.serverHelpers.world.Config.*;
 //import static org.communication.server.serverHelpers.serverHandler.SimpleServer.validCommands;
 
@@ -34,4 +35,44 @@ public class SimpleClientHandler {
         this.robotModels = robotModels;
         this.robotLaunched = false;
     }
+
+    /**
+     * Handles user input commands from the scanner and processes them accordingly.
+     * Commands include launching actions and handling valid commands based on input parts.
+     */
+//    public void handleUserInput(Scanner sc) {
+//        while (Client.keepRunning) {
+//            String userInput = sc.nextLine().toLowerCase();
+//            String[] parts = userInput.split(" ");
+//
+//            if (userInput.startsWith("quit")) {
+//                System.out.println(RED_BRIGHT + "Exiting game..." + RED_BRIGHT);
+//                System.exit(0);
+//            }
+//
+//            try {
+//                if (!Client.repairing && !Client.reloading) {
+//                    if (parts[0].equalsIgnoreCase("launch") && parts.length == 3) {
+//                        handleLaunchCommand(parts);
+//                    } else if(robotLaunched){
+//                        if (validCommands.contains(parts[0])){
+//                            handleValidCommand(parts);
+//                        }else {
+//                            displayInvalidCommandMessage(userInput);
+//                        }
+//                    }else {
+//                        System.out.println(RED_BRIGHT + "Please launch a robot to start!" + RESET);
+//                    }
+//                } else {
+//                    System.out.println(Client.reloading ? YELLOW_BRIGHT + "Busy Reloading..." + RESET : YELLOW_BRIGHT + "Busy Repairing..." + RESET);
+//                }
+//            } catch (Exception e) {
+//                displayInvalidCommandMessage(userInput);
+//            }
+//        }
+//    }
+
+
+
+
 }
