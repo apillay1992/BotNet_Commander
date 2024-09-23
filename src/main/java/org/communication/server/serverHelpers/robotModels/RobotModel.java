0bc0d2486b7ba09ee012837872f2d68cd9398dd7
apillay023 <apillay023@student.wethinkcode.co.za>
2024-09-23 13:39:46 +0200

    created an interface for each robot class to implement these methods

--- src/main/java/org/communication/server/serverHelpers/robotModels/RobotModel.java
+++ src/main/java/org/communication/server/serverHelpers/robotModels/RobotModel.java
@@ -1,4 +1,20 @@
 package org.communication.server.serverHelpers.robotModels;
 
+/**
+ * The RobotModel interface defines the common methods that all robot models should implement.
+ */
 public interface RobotModel {
+    /**
+     * Returns the initial number of shields for the robot model.
+     *
+     * @return The initial number of shields as a String.
+     */
+    String getShield();
+
+    /**
+     * Returns the initial number of shots for the robot model.
+     *
+     * @return The initial number of shots as a String.
+     */
+    String getShots();
 }
