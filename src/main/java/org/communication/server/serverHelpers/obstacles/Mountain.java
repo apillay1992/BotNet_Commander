99e23cd808c4ca7038bf062e8f4213ba4d015c29
apillay023 <apillay023@student.wethinkcode.co.za>
2024-10-01 20:07:52 +0200

    added the obstacle class

--- src/main/java/org/communication/server/serverHelpers/obstacles/Mountain.java
+++ src/main/java/org/communication/server/serverHelpers/obstacles/Mountain.java
@@ -1,45 +1,45 @@
 package org.communication.server.serverHelpers.obstacles;
 
-public class Mountain {
+public class Obstacle {
     private int x;
     private int y;
 
     /**
-     * Constructs a Mountain with the specified x and y coordinates.
+     * Constructs an Obstacle with the specified x and y coordinates.
      *
-     * @param x the x coordinate of the mountain
-     * @param y the y coordinate of the mountain
+     * @param x the x coordinate of the obstacle
+     * @param y the y coordinate of the obstacle
      */
-    public Mountain(int x, int y) {
+    public Obstacle(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
     /**
-     * Returns the x coordinate of the mountain.
+     * Returns the x coordinate of the obstacle.
      *
-     * @return the x coordinate of the mountain
+     * @return the x coordinate of the obstacle
      */
     public int getX() {
         return x;
     }
 
     /**
-     * Returns the y coordinate of the mountain.
+     * Returns the y coordinate of the obstacle.
      *
-     * @return the y coordinate of the mountain
+     * @return the y coordinate of the obstacle
      */
     public int getY() {
         return y;
     }
 
     /**
-     * Returns a string representation of the mountain, including its position.
+     * Returns a string representation of the obstacle, including its position.
      *
-     * @return a string representation of the mountain
+     * @return a string representation of the obstacle
      */
     @Override
     public String toString() {
-        return "Mountain: - At position (" + getX() + ", " + getY() + ")";
+        return "Obstacles: - At position (" + getX() + ", " + getY() + ")";
     }
 }
