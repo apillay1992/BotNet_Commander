8cbcfc8de13994cd08f8f0d12b4abdc07b32bdd5
apillay023 <apillay023@student.wethinkcode.co.za>
2024-10-10 20:44:23 +0200

    added method for to execute fire command

--- src/main/java/org/communication/server/serverHelpers/commands/Fire.java
+++ src/main/java/org/communication/server/serverHelpers/commands/Fire.java
@@ -1,4 +1,24 @@
 package org.communication.server.serverHelpers.commands;
 
-public class Fire {
+import org.communication.server.serverHelpers.robot.Robot;
+
+/**
+ * Represents a command to fire shots from a robot.
+ * Extends the Command class and overrides the execute method to execute the firing action.
+ */
+public class Fire extends Command {
+    public static Robot damagedRobot;
+
+    //Executes the fire command to make the target robot fire shots.
+    @Override
+    public boolean execute(Robot target) {
+        damagedRobot = target.fireShots();
+        return false;
+    }
+
+    //Constructs a fire command with the specified argument.
+    public Fire(String argument){
+        super("fire", argument);
+    }
+
 }
