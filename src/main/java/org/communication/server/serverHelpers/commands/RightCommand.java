c076d809b404977d99519f37ac49a7c8a88e5ae3
apillay023 <apillay023@student.wethinkcode.co.za>
2024-10-10 20:49:41 +0200

    added method for the look command

--- src/main/java/org/communication/server/serverHelpers/commands/RightCommand.java
+++ src/main/java/org/communication/server/serverHelpers/commands/RightCommand.java
@@ -1,4 +1,31 @@
 package org.communication.server.serverHelpers.commands;
 
-public class RightCommand {
+import org.communication.server.serverHelpers.robot.Robot;
+
+public class RightCommand extends Command {
+    /**
+     * Command to turn the robot to the right.
+     */
+    @Override
+    public boolean execute(Robot target) {
+        /**
+         * Executes the command to turn the robot to the right.
+         *
+         * @param target The robot on which to execute the command.
+         * @return true indicating the command was successfully executed.
+         */
+        target.updateDirection(true);
+        target.setStatus("Done");
+        return true;
+    }
+    /**
+     * Constructs a RightCommand with the specified argument.
+     *
+     * @param argument The argument for the right command.
+     */
+    public RightCommand(String argument){
+        super("right", argument);
+    }
+
+
 }
